7f6aa6a491863df33e4783680dc6f32fbad04a26
JUNAID\zigron <junaid.ameer@zigron.com>
2022-07-27 15:01:04 +0500

    commit operation

---
+++ wp-content/plugins/Projects/index.php
@@ -0,0 +1,29 @@
+<?php
+
+    /**
+     * The plugin bootstrap file
+     *
+     * This file is read by WordPress to generate the plugin information in the plugin
+     * admin area. This file also includes all of the dependencies used by the plugin,
+     * registers the activation and deactivation functions, and defines a function
+     * that starts the plugin.
+     *
+     * @link
+     * @since             1.0.0
+     *
+     * @wordpress-plugin
+     * Plugin Name:       Projects
+     * Plugin URI:
+     * Description:       Plugin
+     * Version:           1.0.0
+     * Author:            Junaid Ameer
+     * Author URI:        https://www.linkedin.com/in/junaidameer/
+     * License:           GPL-2.0+
+     * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+     * Text Domain:       _th_
+     */
+
+    require plugin_dir_path( __FILE__ ) . 'includes/plugin-core.php';
+
+
+?>
